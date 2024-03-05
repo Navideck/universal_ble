ae6169a668df67f17c4e43356b7256d813c2d4ce
Rohit Sangwan <rohitsangwan647@gmail.com>
2024-03-05 22:28:43 +0530

    Fix UUID parsing

--- lib/src/models/uuid.dart
+++ lib/src/models/uuid.dart
@@ -2,28 +2,25 @@ import 'package:bluez/bluez.dart';
 
 class UUID {
   late String value;
-  late BlueZUUID _blueZUUID;
 
   UUID(String uuid) {
-    if (uuid.length < 8) {
+    // To validate the short UUID
+    if (uuid.length <= 4) {
       try {
         int shortValue = int.parse(uuid, radix: 16);
-        _blueZUUID = BlueZUUID.short(shortValue);
-      } catch (e) {
-        _blueZUUID = BlueZUUID.fromString(uuid);
-      }
-    } else {
-      _blueZUUID = BlueZUUID.fromString(uuid);
+        value = BlueZUUID.short(shortValue).toString();
+        return;
+      } catch (_) {}
     }
-    value = _blueZUUID.toString();
+    // To validate the UUID
+    BlueZUUID.fromString(uuid);
+    value = uuid;
   }
 
   factory UUID.fromShort(int short) {
     BlueZUUID blueZUUID = BlueZUUID.short(short);
     return UUID(blueZUUID.toString());
   }
-
-  bool get isShort => _blueZUUID.isShort;
 
   @override
   String toString() {
