7eb281264f61d90662af2485baa61a60934a1abe
fotiDim <foti@navideck.com>
2024-03-18 17:17:48 +0000

    Deploying to gh-pages from @ Navideck/universal_ble@580fca6e4a45ef715a0ce6aa1cdda817fcf9d4f4 🚀

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,8 +3,8 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"index.html": "6c11be891d78925b5538283bb9e3f10a",
-"/": "6c11be891d78925b5538283bb9e3f10a",
+const RESOURCES = {"index.html": "bb792b2e828ed78c8c392572c15145da",
+"/": "bb792b2e828ed78c8c392572c15145da",
 "canvaskit/canvaskit.js": "eb8797020acdbdf96a12fb0405582c1b",
 "canvaskit/skwasm.js": "87063acf45c5e1ab9565dcf06b0c18b8",
 "canvaskit/skwasm.wasm": "4124c42a73efa7eb886d3400a1ed7a06",
@@ -28,7 +28,7 @@ const RESOURCES = {"index.html": "6c11be891d78925b5538283bb9e3f10a",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "manifest.json": "7710138ad501c15f400cc9b3504e8b0a",
 "flutter.js": "7d69e653079438abfbb24b82a655b0a4",
-"main.dart.js": "ae55a816302a4ac753a44b06f08b5a7a"};
+"main.dart.js": "eae8674be3758a717ccc5c07dd2885c7"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
