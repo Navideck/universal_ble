7fd4fdf89483b9a9d8ced62e78540ffb4231bd13
fotiDim <foti@navideck.com>
2024-09-24 15:34:40 +0000

    Deploying to gh-pages from @ Navideck/universal_ble@491468728463bca6e0d84f51b0c79f54c949fcbc 🚀

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -26,7 +26,7 @@ const RESOURCES = {"assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
-"flutter_bootstrap.js": "f43a61c38eba9f073757de8ef6f66c46",
+"flutter_bootstrap.js": "361a127c30d9cc97f1145455dafce731",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
 "index.html": "35f818301518cad926b9158461da527b",
