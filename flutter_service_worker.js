6517c57fd6eefe809ffc8deb49b536c72046f85e
rohitsangwan01 <rohitsangwan647@gmail.com>
2024-10-03 06:24:28 +0000

    Deploying to gh-pages from @ Navideck/universal_ble@aeee6ad2e47b1e7dd7834b2ce58eeb5d8af0e9e9 🚀

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -26,7 +26,7 @@ const RESOURCES = {"assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
-"flutter_bootstrap.js": "85320f53f27b1de77a40fc7adcf11999",
+"flutter_bootstrap.js": "51122a7babfab35f749075ebf4ac783e",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
 "index.html": "35f818301518cad926b9158461da527b",
