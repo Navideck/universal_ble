99dae7f6645f40f5a44430c8c9b8266b7c86990b
fotiDim <foti@navideck.com>
2024-12-05 07:27:19 +0000

    Deploying to gh-pages from @ Navideck/universal_ble@7542de8242951bd8a6f9cacf6983f536a2374d16 🚀

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -32,7 +32,7 @@ const RESOURCES = {"flutter.js": "f393d3c16b631f36852323de8e583132",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "version.json": "f00e9d5f2db8bbae1b3286ac023cb7d6",
-"flutter_bootstrap.js": "2c32610f575efc5367baeaf8fc8619c1"};
+"flutter_bootstrap.js": "d7335df34579c5aee0e63331cb827a47"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
