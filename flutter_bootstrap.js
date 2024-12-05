b7159b83517e8bdb449a076db047e819ac6f6b3e
rohitsangwan01 <rohitsangwan647@gmail.com>
2024-12-05 07:59:23 +0000

    Deploying to gh-pages from @ Navideck/universal_ble@61e1448fe9e4f61c550fc713b86b2c5a5bec7104 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1494263345"
+    serviceWorkerVersion: "3039059493"
   }
 });
