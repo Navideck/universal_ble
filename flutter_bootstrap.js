74c768289efa8fc887ba755036335534dc73e86e
fotiDim <foti@navideck.com>
2025-06-12 22:31:58 +0000

    Deploying to gh-pages from @ Navideck/universal_ble@72090571a37ce17691cca4aff173c298899a0d84 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -34,11 +34,11 @@ addEventListener("message", eventListener);
 if (!window._flutter) {
   window._flutter = {};
 }
-_flutter.buildConfig = {"engineRevision":"1425e5e9ec5eeb4f225c401d8db69b860e0fde9a","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js"}]};
+_flutter.buildConfig = {"engineRevision":"31c4875c7aa0079ae11a4850e2d06bd83d001960","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js"}]};
 
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3101640444"
+    serviceWorkerVersion: "2895620252"
   }
 });
