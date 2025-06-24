39e61403ef7f047b786a22631d00e4fe7bcadd93
fotiDim <foti@navideck.com>
2025-06-24 13:14:02 +0000

    Deploying to gh-pages from @ Navideck/universal_ble@17e13a475d3cad2d50772818afdac62db8aa20de 🚀

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -30,7 +30,7 @@ const RESOURCES = {"canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"flutter_bootstrap.js": "b955fd81ad30bdd464edcfeef6db4716",
+"flutter_bootstrap.js": "63821a0b03b758213fb49b8cac74927c",
 "version.json": "f00e9d5f2db8bbae1b3286ac023cb7d6"};
 // The application shell files that are downloaded before a service worker can
 // start.
