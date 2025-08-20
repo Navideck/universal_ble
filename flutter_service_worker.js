1fbaecb695fcb3d349627f3bef0243c321dce9bd
fotiDim <foti@navideck.com>
2025-08-20 07:08:50 +0000

    Deploying to gh-pages from @ Navideck/universal_ble@49a9d1defb262a353d75f1f3f03dbbdfd5ff6e8e 🚀

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "bef692456c9712f1f955a41d7e68d1d6",
+const RESOURCES = {"flutter_bootstrap.js": "6423ee99ab2ee9a11621c1ae9c1090fb",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
