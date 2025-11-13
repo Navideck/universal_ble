1e80f9b3c4c564d7334cdee38c88382267e42503
fotiDim <foti@navideck.com>
2025-11-13 16:24:18 +0000

    Deploying to gh-pages from @ Navideck/universal_ble@970db165dd9ab271c1412e6684e909a91281d55c 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"cb467e31a54207dc987aca5bb0941c4d2e3fd9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3773078582"
+    serviceWorkerVersion: "2504648208"
   }
 });
